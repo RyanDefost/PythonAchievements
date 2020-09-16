0f02035f404e57f57e405b155c8b800efa7ee6f9
Ryan de Fost <ryandefost@hotmail.com>
2020-09-16 12:46:06 +0200

    Eerste commit text_based_adventure

---
+++ Text_based_adventure.py
@@ -0,0 +1,16 @@
+print("Hallo ik ben ERROR en jij?")
+naam = (input())
+print("Hallo " + naam)
+
+print("Aan welk getal denk ik van 1 tot 5?")
+antwoord1 = input()
+
+else: print("Dit is geen mogelijk antwoord probeer het opnieuw")
+antwoord1 = input()
+if antwoord1 == "1": print ("Ja inderdaad")
+if antwoord1 == "2": print ("Nee helaas")
+if antwoord1 == "3": print ("Nee helaas")
+if antwoord1 == "4": print ("Nee helaas")
+if antwoord1 == "5": print ("Nee helaas")
+
+
