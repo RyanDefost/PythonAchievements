ceebb4e3c03111b84197d3db7ce8d3dce3b44ab3
Ryan de Fost <ryandefost@hotmail.com>
2020-09-18 12:55:02 +0200

    playerStatistic toegevoegt

---
+++ PYTB1L2PlayerStatistics.py
@@ -0,0 +1,15 @@
+
+Naam = "tien"
+leeftijd = 19
+Levens = 3
+specialiteit = "voetbal"
+levend = True
+
+print("Naam:" + Naam)
+print("specialiteit:" + specialiteit)
+print("Age:")
+print(leeftijd)
+print("levens:")
+print(Levens)
+print("Levend:")
+print(levend)
