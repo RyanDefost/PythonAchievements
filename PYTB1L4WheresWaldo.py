df30ba5c1070de49ec1b70c016f4031427dc2410
Ryan de Fost <ryandefost@hotmail.com>
2020-09-25 13:06:30 +0200

    Where is waldo toegevoegt

---
+++ PYTB1L4WheresWaldo.py
@@ -0,0 +1,16 @@
+#Zoeken naar Waldo
+#
+
+import random
+people = ["Waldo","Jackson","Ray","Eric","Vaughn","Jesse","Herbert","Robert", "Rodrigo","Elija","Sasha","Nathaniel","Ellie","Allison","Jeremiah", "Paula", "Alisha","Tory","Troy", "Faye"]
+random.shuffle(people)
+
+
+stoelnummer = -1
+for x in range(len(people)):
+    if people[x] == "Waldo":
+        stoelnummer = x
+
+print(people)
+print("waldo zit in stoel Nr." + str(stoelnummer))
+
